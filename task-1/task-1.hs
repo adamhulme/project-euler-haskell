cfedc83aa496f48ba2b2e2dbcaef7212df20efd7
Adam Hulme <adamhulme4@gmail.com>
2025-07-26 18:54:55 +0100

    Refactor (add isDivisibleAny)

--- task-1/task-1.hs
+++ task-1/task-1.hs
@@ -1,5 +1,7 @@
 divisible n x = x `mod` n == 0
 divisible2 m n x = x `mod` m == 0 || x `mod` n == 0
+isDivisibleAny :: [Int] -> Int -> Bool
+isDivisibleAny nums x = any (\n -> x `mod` n == 0) nums
 
 main :: IO ()
-main = putStrLn (show (sum (filter (\x -> divisible2 3 5 x) [1..999])))
+main = putStrLn (show (sum (filter (\x -> isDivisibleAny [3,5] x) [1..999])))
